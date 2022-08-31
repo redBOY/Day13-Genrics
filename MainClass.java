8bcfd368f9d84c1ef744b682c1045fcf4c6c8949
“Adithya <adithyasrinivas66@gmail.com>
2022-09-01 00:40:54 +0530

    uc3 commit

--- MainClass.java
+++ MainClass.java
@@ -3,15 +3,16 @@ package com.bridgelabz_genrics;
 public class MainClass {
     public static void main(String[] args) {
 
-        MaxFloat maxFloat  = new MaxFloat();
+        MaxString maxString  = new MaxString();
         System.out.println("Test case 1");
-        maxFloat.maxValue(2.8F,4.4F,33.2F);
+        maxString.maxValue("Sun","Moon","Stars");
         System.out.println("Test case 2");
-        maxFloat.maxValue(62.5F,77.6F,88.8F);
+        maxString.maxValue("Orange","Mango","Kiwi");
         System.out.println("Test case 3");
-        maxFloat.maxValue(99.9F,55.6F,8.3F);
+        maxString.maxValue("Boy","Girl","Man");
     }
 }
+
 
 
 
