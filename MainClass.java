ce351510c795f869a81eb28c57ce4dd728dcf994
“Adithya <adithyasrinivas66@gmail.com>
2022-09-01 01:55:53 +0530

    refactor1

--- MainClass.java
+++ MainClass.java
@@ -2,16 +2,28 @@ package com.bridgelabz_genrics;
 
 public class MainClass {
     public static void main(String[] args) {
-
-        MaxString maxString  = new MaxString();
+        MaxValue maxvalue = new MaxValue();
         System.out.println("Test case 1");
-        maxString.maxValue("Sun","Moon","Stars");
+        intMax.maxValue(1, 3, 5);
         System.out.println("Test case 2");
-        maxString.maxValue("Orange","Mango","Kiwi");
+        intMax.maxValue(2, 4, 6);
         System.out.println("Test case 3");
-        maxString.maxValue("Boy","Girl","Man");
+        intMax.maxValue(9, 10, 11);
+        System.out.println("Test case 1");
+        maxFloat.maxValue(2.8F, 4.4F, 33.2F);
+        System.out.println("Test case 2");
+        maxFloat.maxValue(62.5F, 77.6F, 88.8F);
+        System.out.println("Test case 3");
+        maxFloat.maxValue(99.9F, 55.6F, 8.3F);
+        System.out.println("Test case 1");
+        maxString.maxValue("Sun", "Moon", "Stars");
+        System.out.println("Test case 2");
+        maxString.maxValue("Orange", "Mango", "Kiwi");
+        System.out.println("Test case 3");
+        maxString.maxValue("Boy", "Girl", "Man");
     }
 }
+
 
 
 
--- MaxValue.java
+++ MaxValue.java
@@ -1,16 +1,21 @@
 package com.bridgelabz_genrics;
-public class MaxValue {
 
-    void maxValue(Integer val1, Integer val2, Integer val3) {
-        if ((val1.compareTo(val2)) >= 0 && (val1.compareTo(val3)) >= 0)
-            System.out.println(val1 + " is the largest");
+    public class MaxValue {
+        public <K extends Comparable <K>> void maxValue(K val1, K val2, K val3){
+            if((val1.compareTo(val2)) >=0 && (val1.compareTo(val3)) >=0)
+                System.out.println(val1+ " is the largest: ");
 
-        else if ((val2.compareTo(val1)) >= 0 && (val2.compareTo(val3)) >= 0) {
-            System.out.println(val2 + " is largest");
-        } else {
-            System.out.println(val3 + " is largest");
+            else if ((val2.compareTo(val1)) >=0 && (val2.compareTo(val3)) >=0) {
+                System.out.println(val2+" is largest: ");
+            }
+            else{
+                System.out.println(val3+" is largest: ");
+
+            }
 
         }
-
     }
-}
+
+
+
+
